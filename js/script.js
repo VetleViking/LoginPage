6b3810a0b192a6d1269adcfbac4603161c632e87
VetleViking <vetlelind@gmail.com>
2024-01-19 11:05:08 +0100

    eee

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 async function getUser(username) {
-    let retUser = {};
-    await fetch('./users.json').then(response => {
-        return response.json();
-    }).then(data => {
-        let users = data.users;
+  let retUser = {};
+  await fetch("./users.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let users = data.users;
 
-        users.forEach(user => {
-            if (user.username === username) {
-                retUser = user;
-                return
-            }
-        });   
-        
-    }).catch(err => {
-        console.log(err);
+      users.forEach((user) => {
+        if (user.username === username) {
+          retUser = user;
+          return;
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-    return retUser;
+  return retUser;
 }
+
+const css_code = document.getElementById("css-code");
+const body = document.getElementById("body");
+
+function run() {
+  body.contentDocument.body.innerHTML = `<style>${css_code.value}</style>`;
+}
+
+css_code.onkeyup = () => run();
